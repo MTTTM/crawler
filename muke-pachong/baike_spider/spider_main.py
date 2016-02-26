ebccb12119b16c9efba164345d44bc89b9b59d97
MTTTM <13352840347@163.com>
2016-02-27 01:04:02 +0800

    报错注析

--- muke-pachong/baike_spider/spider_main.py
+++ muke-pachong/baike_spider/spider_main.py
@@ -1,6 +1,10 @@
  #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from baike_spider import url_manger,html_downloader,html_parser,html_outputer
+# Traceback (most recent call last):
+#   File "D:/py/muke-pachong/baike_spider/spider_main.py", line 3, in <module>
+#     from baike_spider import url_manger,html_downloader,html_parser,html_outputer
+# ImportError: No module named baike_spider
 class SpiderMain(object):
     def __init__(self):
         self.urls=url_manger.UrlMangager() #url管理器
